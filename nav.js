ce80945236fb767acd229bb8282cb470fc512f97
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2023-03-28 11:05:27 +0530

    Most of the frontend is done! only remaining part is that if we click on primary navbar it should open a full screen list and this should be done in hamburger also
    Now it's time for backend!

--- nav.js
+++ nav.js
@@ -1,14 +1,14 @@
-const primaryNav = document.querySelector(".primary-navigation")
+const navBar = document.querySelector(".navbar")
 const navToggle = document.querySelector(".mobile-nav-toggle")
 
-navToggle.addEventListener('click',()=>{
-    const visibility=primaryNav.getAttribute('data-visible')
-    if(visibility==="false"){
-        primaryNav.setAttribute('data-visible', true);
+navToggle.addEventListener('click', () => {
+    const visibility = navBar.getAttribute('data-visible')
+    if (visibility === "false") {
+        navBar.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
     }
-    else{
-        primaryNav.setAttribute('data-visible', false);
+    else {
+        navBar.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
     }
 })
