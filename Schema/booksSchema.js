4548ee522cfe7e7d776e8265396512c5477172c5
ManikantaMandala <mandala.manikanta.20cse@bmu.edu.in>
2023-04-04 13:38:08 +0530

    Added server(routes), wrote some schemas and connected to database

---
+++ Schema/booksSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const SchemaTypes = mongoose.Schema.Types;
+const bookSchema = new mongoose.Schema({
+    ISBN:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    title:{
+        type: String,
+        required: true,
+    },
+    author: [{type: String}],
+    contributor: [{type: String}],
+    publication:String,
+    edition:Number,
+    ddc: {
+        type:SchemaTypes.Decimal128,
+        required: true,
+        
+    },
+    rating: SchemaTypes.Decimal128,
+    Subject: [{type: String}]
+})
+
+module.exports = mongoose.Schema("Books",bookSchema)
